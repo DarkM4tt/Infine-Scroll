19019d728c9b088b043f08b638d54ca2697e7502
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2022-06-26 23:25:44 +0530

    Created Infinite Scroll With React.

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css'
+import { useEffect, useState } from 'react'
+
+const PAGE_NUMBER = 1
+
+function App() {
+  const [state, setState] = useState([])
+  const [page, setPage] = useState(PAGE_NUMBER)
+
+  useEffect(() => {
+    fetch(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=5`)
+      .then((res) => res.json())
+      .then((json) => setState([...state, ...json.data]))
+  })
+
+  const scrollToEnd = () => {
+    setPage(page + 1)
+  }
+
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      scrollToEnd()
+    }
+  }
+
+  return (
+    <div className="App">
+      {state.length > 0 &&
+        state.map((el, i) => (
+          <div key={i} className={'container'}>
+            <h4>Id: {el._id}</h4>
+            <h4>Name: {el.name}</h4>
+            <h4>Trips: {el.trips}</h4>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export default App
